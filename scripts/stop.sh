1cf722670ed6514926e542a225fa30dd32869112
Giulio <callegarin.giulio02@gmail.com>
2024-06-27 11:35:24 +0200

    updated stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 cd /home/ubuntu/tonyapp
-docker stop tonyapp
-docker remove tonyapp
-docker image remove tonyapp:latest
+
+# Check if the container is running and stop it if it is
+if [ "$(docker ps -q -f name=tonyapp)" ]; then
+    docker stop tonyapp
+fi
+
+# Check if the container exists and remove it if it does
+if [ "$(docker ps -a -q -f name=tonyapp)" ]; then
+    docker rm tonyapp
+fi
+
+# Check if the image exists and remove it if it does
+if [ "$(docker images -q tonyapp:latest)" ]; then
+    docker rmi tonyapp:latest
+fi
